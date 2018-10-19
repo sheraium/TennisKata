650492c4a50ffc8ae6019d36957a15c824aa08ec
320陳永修 <peterch@mirle.com.tw>
2018-10-19 23:23:14 +0800

    pass

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -2,14 +2,20 @@ namespace TennisKata
 {
     public class TennisGame
     {
+        private int _firstPlayerScoreTimes;
+
         public string Score()
         {
+            if (_firstPlayerScoreTimes == 1)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
         }
 
         public void FirstPlayerScore()
         {
-            throw new System.NotImplementedException();
+            _firstPlayerScoreTimes++;
         }
     }
 }
