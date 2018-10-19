5d8031021112d83707df10e2740f5278c737c78c
320陳永修 <peterch@mirle.com.tw>
2018-10-19 23:29:42 +0800

    refactoring

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -1,22 +1,23 @@
+using System.Collections.Generic;
+
 namespace TennisKata
 {
     public class TennisGame
     {
         private int _firstPlayerScoreTimes;
 
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
+            {
+                { 1,"Fifteen"},
+                {2,"Thirty" },
+                {3,"Forty" },
+            };
+
         public string Score()
         {
-            if (_firstPlayerScoreTimes == 3)
+            if (_firstPlayerScoreTimes > 0)
             {
-                return "Forty Love";
-            }
-            if (_firstPlayerScoreTimes == 2)
-            {
-                return "Thirty Love";
-            }
-            if (_firstPlayerScoreTimes == 1)
-            {
-                return "Fifteen Love";
+                return _scoreLookup[_firstPlayerScoreTimes] + " Love";
             }
             return "Love All";
         }
