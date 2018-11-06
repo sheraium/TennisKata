b2397334ac421efd248a1d9a98aeb8cd16f848ac
320 陳永修 <peterch@mirle.com.tw>
2018-11-06 21:50:00 +0800

    love thirty

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -41,6 +41,14 @@ namespace TennisKata
             ScoreShouldBe("Love Fifteen");
         }
 
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            tennisGame.SecondPlayerScore();
+            tennisGame.SecondPlayerScore();
+            ScoreShouldBe("Love Thirty");
+        }
+
         private void GivenFirstPlayerScore(int times)
         {
             for (int i = 0; i < times; i++)
