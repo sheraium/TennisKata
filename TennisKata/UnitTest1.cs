9a4cd831412683936ef72e770ff82e3496385595
320 陳永修 <peterch@mirle.com.tw>
2018-11-06 21:47:09 +0800

    refactoring

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -23,9 +23,16 @@ namespace TennisKata
         [TestMethod]
         public void Thirty_Love()
         {
-            tennisGame.FirstPlayerScore();
-            tennisGame.FirstPlayerScore();
+            GivenFirstPlayerScore(2);
             ScoreShouldBe("Thirty Love");
+        }
+
+        private void GivenFirstPlayerScore(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                tennisGame.FirstPlayerScore();
+            }
         }
 
         private void ScoreShouldBe(string expected)
