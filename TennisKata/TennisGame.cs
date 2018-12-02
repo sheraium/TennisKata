2641767e29db734027599e8fb529a817326ad4ae
sheraium <sheraium@gmail.com>
2018-12-02 17:00:04 +0800

    tom adv

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -31,8 +31,7 @@ namespace TennisKata
             {
                 if (_firstPlayerScoreTimes >= 3 && _secondPlayerScoreTimes >= 3)
                 {
-                    if (Math.Abs(_firstPlayerScoreTimes - _secondPlayerScoreTimes) == 1
-                        && _firstPlayerScoreTimes != 5 && _secondPlayerScoreTimes != 5)
+                    if (IsAdv())
                     {
                         return _firstPlayer + " Adv";
                     }
@@ -41,6 +40,12 @@ namespace TennisKata
                 return NormalScore();
             }
             return IsDeuce() ? Deuce() : SameScore();
+        }
+
+        private bool IsAdv()
+        {
+            return Math.Abs(_firstPlayerScoreTimes - _secondPlayerScoreTimes) == 1
+                   && _firstPlayerScoreTimes != 5 && _secondPlayerScoreTimes != 5;
         }
 
         private static string Deuce()
