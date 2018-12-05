a457126a396fdb987185646b212d84e26e812eb2
sheraium <sheraium@gmail.com>
2018-12-05 22:18:11 +0800

    deuce 4 4

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -70,6 +70,14 @@ namespace TennisKata
             GivenSecondPlayerScoreTimes(3);
             ScoreShouldBe("Deuce");
         }
+
+        [TestMethod]
+        public void Deuce_when_4_4()
+        {
+            GivenFirstPlayerScoreTimes(4);
+            GivenSecondPlayerScoreTimes(4);
+            ScoreShouldBe("Deuce");
+        }
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
