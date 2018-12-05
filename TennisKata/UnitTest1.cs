8eae7068875eefd291013da450cd8d2d4485fcc8
sheraium <sheraium@gmail.com>
2018-12-05 22:15:58 +0800

    thirty all

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -54,6 +54,14 @@ namespace TennisKata
             GivenSecondPlayerScoreTimes(1);
             ScoreShouldBe("Fifteen All");
         }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty All");
+        }
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
