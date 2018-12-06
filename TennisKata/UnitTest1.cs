1e5846da93178ca2f94d6cf66e859fccee5eb2eb
sheraium <sheraium@gmail.com>
2018-12-06 19:36:40 +0800

    love all

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -5,124 +5,12 @@ namespace TennisKata
     [TestClass]
     public class UnitTest1
     {
-        TennisGame tennisGame = new TennisGame("Joey", "Tom");
         [TestMethod]
         public void Love_All()
         {
-            ScoreShouldBe("Love All");
-        }
-
-        [TestMethod]
-        public void Fifteen_Love()
-        {
-            tennisGame.FirstPlayerScore();
-            ScoreShouldBe("Fifteen Love");
-        }
-
-        [TestMethod]
-        public void Thirty_Love()
-        {
-            GivenFirstPlayerScoreTimes(2);
-            ScoreShouldBe("Thirty Love");
-        }
-
-        [TestMethod]
-        public void Forty_Love()
-        {
-            GivenFirstPlayerScoreTimes(3);
-            ScoreShouldBe("Forty Love");
-        }
-
-        [TestMethod]
-        public void Love_Fifteen()
-        {
-            tennisGame.SecondPlayerScore();
-            ScoreShouldBe("Love Fifteen");
-        }
-
-        [TestMethod]
-        public void Love_Thirty()
-        {
-            GivenSecondPlayerScoreTimes(2);
-            ScoreShouldBe("Love Thirty");
-        }
-
-        [TestMethod]
-        public void Fifteen_All()
-        {
-            GivenFirstPlayerScoreTimes(1);
-            GivenSecondPlayerScoreTimes(1);
-            ScoreShouldBe("Fifteen All");
-        }
-
-        [TestMethod]
-        public void Thirty_All()
-        {
-            GivenFirstPlayerScoreTimes(2);
-            GivenSecondPlayerScoreTimes(2);
-            ScoreShouldBe("Thirty All");
-        }
-
-        [TestMethod]
-        public void Deuce()
-        {
-            GivenFirstPlayerScoreTimes(3);
-            GivenSecondPlayerScoreTimes(3);
-            ScoreShouldBe("Deuce");
-        }
-
-        [TestMethod]
-        public void Deuce_when_4_4()
-        {
-            GivenFirstPlayerScoreTimes(4);
-            GivenSecondPlayerScoreTimes(4);
-            ScoreShouldBe("Deuce");
-        }
-
-        [TestMethod]
-        public void Joey_Adv()
-        {
-            GivenFirstPlayerScoreTimes(4);
-            GivenSecondPlayerScoreTimes(3);
-            ScoreShouldBe("Joey Adv");
-        }
-
-        [TestMethod]
-        public void Tom_Adv()
-        {
-            GivenFirstPlayerScoreTimes(3);
-            GivenSecondPlayerScoreTimes(4);
-            ScoreShouldBe("Tom Adv");
-        }
-
-        [TestMethod]
-        public void Tom_Win()
-        {
-            GivenFirstPlayerScoreTimes(3);
-            GivenSecondPlayerScoreTimes(5);
-            ScoreShouldBe("Tom Win");
-        }
-        private void GivenSecondPlayerScoreTimes(int times)
-        {
-            for (int i = 0; i < times; i++)
-            {
-
-                tennisGame.SecondPlayerScore();
-            }
-        }
-
-        private void GivenFirstPlayerScoreTimes(int times)
-        {
-            for (int i = 0; i < times; i++)
-            {
-                tennisGame.FirstPlayerScore();
-            }
-        }
-
-        private void ScoreShouldBe(string expected)
-        {
+            TennisGame tennisGame = new TennisGame();
             var score = tennisGame.Score();
-            Assert.AreEqual(expected, score);
+            Assert.AreEqual("Love All", score);
         }
     }
 }
