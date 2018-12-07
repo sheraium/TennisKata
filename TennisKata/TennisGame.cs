79bafd56ab2c856f8d50bf273c25118b81e7496d
sheraium <sheraium@gmail.com>
2018-12-07 22:04:11 +0800

    pass

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -34,7 +34,9 @@ namespace TennisKata
                 {
                     if (IsAdv())
                     {
-                        return _firstPlayer + " Adv";
+                        var advPlayer = _firstPlayerScoreTimes > _secondPlayerScoreTimes
+                                       ? _firstPlayer : _secondPlayer;
+                        return advPlayer + " Adv";
                     }
                 }
 
