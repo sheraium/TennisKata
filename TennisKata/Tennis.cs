ca8dbb869968790796b747b4bf8a2364dac1209c
320 陳永修 <peterch@mirle.com.tw>
2018-12-13 20:53:56 +0800

    pass

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -8,6 +8,7 @@ namespace TennisKata
 
         private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
             {
+                {0,"Love" },
                 {1,"Fifteen" },
                 {2,"Thirty" },
                 {3,"Forty" },
@@ -17,17 +18,9 @@ namespace TennisKata
 
         public string Score()
         {
-            if (_secondPlayerScoreTimes == 2)
+            if (_firstPlayerScoreTimes != _secondPlayerScoreTimes)
             {
-                return "Love Thirty";
-            }
-            if (_secondPlayerScoreTimes == 1)
-            {
-                return "Love Fifteen";
-            }
-            if (_firstPlayerScoreTimes > 0)
-            {
-                return _scoreLookup[_firstPlayerScoreTimes] + " Love";
+                return _scoreLookup[_firstPlayerScoreTimes] + " " + _scoreLookup[_secondPlayerScoreTimes];
             }
 
             return "Love All";
