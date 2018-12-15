21e7348d9676fd42392bd925cbe65a95e02463ee
320 陳永修 <peterch@mirle.com.tw>
2018-12-15 22:40:31 +0800

    love all

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -1,87 +1,10 @@
-using System;
-using System.Collections.Generic;
-
 namespace TennisKata
 {
     public class Tennis
     {
-        private readonly string _firstPlayer;
-        private readonly string _secondPlayer;
-        private int _firstPlayerScoreTimes;
-
-        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
-            {
-                {0,"Love" },
-                {1,"Fifteen" },
-                {2,"Thirty" },
-                {3,"Forty" },
-            };
-
-        private int _secondPlayerScoreTimes;
-
-        public Tennis(string firstPlayer, string secondPlayer)
-        {
-            _firstPlayer = firstPlayer;
-            _secondPlayer = secondPlayer;
-        }
-
         public string Score()
         {
-            return IsDifferent() ? IsReadyForWin() ? IsAdv() ? $"{AdvPlayer()} Adv" : $"{AdvPlayer()} Win" :
-                NormalScore() :
-                IsDeuce() ? Deuce() : SameScore();
-        }
-
-        private string AdvPlayer()
-        {
-            return _firstPlayerScoreTimes > _secondPlayerScoreTimes
-                ? _firstPlayer : _secondPlayer;
-        }
-
-        private bool IsAdv()
-        {
-            return Math.Abs(_firstPlayerScoreTimes - _secondPlayerScoreTimes) == 1
-                   && _firstPlayerScoreTimes != 5 && _secondPlayerScoreTimes != 5;
-        }
-
-        private bool IsReadyForWin()
-        {
-            return _firstPlayerScoreTimes >= 3 && _secondPlayerScoreTimes >= 3;
-        }
-
-        private static string Deuce()
-        {
-            return "Deuce";
-        }
-
-        private bool IsDeuce()
-        {
-            return _firstPlayerScoreTimes >= 3;
-        }
-
-        private string SameScore()
-        {
-            return $"{_scoreLookup[_firstPlayerScoreTimes]} All";
-        }
-
-        private bool IsDifferent()
-        {
-            return _firstPlayerScoreTimes != _secondPlayerScoreTimes;
-        }
-
-        private string NormalScore()
-        {
-            return $"{_scoreLookup[_firstPlayerScoreTimes]} {_scoreLookup[_secondPlayerScoreTimes]}";
-        }
-
-        public void FirstPlayerScoreTimes()
-        {
-            _firstPlayerScoreTimes++;
-        }
-
-        public void SecondPlayerScore()
-        {
-            _secondPlayerScoreTimes++;
+            throw new System.NotImplementedException();
         }
     }
 }
