88c661ed559d59acd45400b3dc6fdbc7846ba356
320 陳永修 <peterch@mirle.com.tw>
2019-01-19 21:36:03 +0800

    deuce 4 4

--- TennisKata/TennisTests.cs
+++ TennisKata/TennisTests.cs
@@ -72,6 +72,14 @@ namespace TennisKata
             ScoreShouldBe("Deuce");
         }
 
+        [TestMethod]
+        public void Deuce_when_4_4()
+        {
+            GivenFirstPlayerScoreTimes(4);
+            GivenSecondPlayerScoreTimes(4);
+            ScoreShouldBe("Deuce");
+        }
+
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (var i = 0; i < times; i++)
