4bf133f5da4ec2f3feea965d19041ff617219b56
320 陳永修 <peterch@mirle.com.tw>
2019-02-19 23:11:07 +0800

    thirty all

--- TennisKata/TennisTests.cs
+++ TennisKata/TennisTests.cs
@@ -56,6 +56,14 @@ namespace TennisKata
             ScoreShouldBe("Fifteen All");
         }
 
+        [TestMethod]
+        public void Thirty_All()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty All");
+        }
+
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
