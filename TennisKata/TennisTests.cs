d51e86374d7c54274f554977fb465cfc0275b4fc
320 陳永修 <peterch@mirle.com.tw>
2019-02-19 22:34:45 +0800

    fifteen love

--- TennisKata/TennisTests.cs
+++ TennisKata/TennisTests.cs
@@ -13,6 +13,13 @@ namespace TennisKata
             ScoreShouldBe("Love All");
         }
 
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            _tennis.FirstPlayerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
         private void ScoreShouldBe(string expected)
         {
             var score = _tennis.Score();
