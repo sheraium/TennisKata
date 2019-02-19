2ce0a9f824452c512d218b0d1906cfda1d4b18b4
320 陳永修 <peterch@mirle.com.tw>
2019-02-19 23:14:26 +0800

    refactor

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -23,15 +23,8 @@ namespace TennisKata
                 return $"{_scoreLookup[_firstPlayerScoreTimes]} {_scoreLookup[_secondPlayerScoreTimes]}";
             }
 
-            if (_firstPlayerScoreTimes == 2)
-            {
-                return "Thirty All";
-            }
-            if (_firstPlayerScoreTimes == 1)
-            {
-                return "Fifteen All";
-            }
-            return "Love All";
+            return _scoreLookup[_firstPlayerScoreTimes] +
+                   " All";
         }
 
         public void FirstPlayerScore()
